f7529a48009f290e9486adf25e9c71b097e5499c
Yannik hofer <133002839+yaenu4599@users.noreply.github.com>
2026-07-13 12:05:34 +0000

    fix: dumb problem
    still having elements in aws_instance only the network interface should have gives conflicts

--- modules/compute/ec2instance/main.tf
+++ modules/compute/ec2instance/main.tf
@@ -21,8 +21,6 @@ resource "aws_instance" "main" {
   count                       = length(var.subnet_ids)
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  vpc_security_group_ids      = var.security_group_ids
-  subnet_id                   = var.subnet_ids[count.index]
   # key_name                    = aws_key_pair.main.key_name
   
   primary_network_interface {
