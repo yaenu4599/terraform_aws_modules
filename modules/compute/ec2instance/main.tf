cd4152dc235548acadf2dabafd08238821ab0e50
Yannik hofer <133002839+yaenu4599@users.noreply.github.com>
2026-07-13 11:51:02 +0000

    creating an eni to be used for the instance
    still hat a problem with the request tags and it created the eni without any tags
    now it creates the eni in advance with the correct tags.

--- modules/compute/ec2instance/main.tf
+++ modules/compute/ec2instance/main.tf
@@ -6,6 +6,17 @@ resource "aws_key_pair" "main" {
 }
 */
 
+resource "aws_network_interface" "main" {
+  count = length(var.subnet_ids)
+  subnet_id = var.subnet_ids[count.index]
+  security_groups = var.security_group_ids
+  source_dest_check = true
+
+  tags = merge( var.common_tags, {
+    Name = "${var.environment}-instance-eni"
+  })
+}
+
 resource "aws_instance" "main" {
   count                       = length(var.subnet_ids)
   ami                         = var.ami_id
@@ -14,11 +25,16 @@ resource "aws_instance" "main" {
   subnet_id                   = var.subnet_ids[count.index]
   associate_public_ip_address = false
   # key_name                    = aws_key_pair.main.key_name
-
+  
+  primary_network_interface {
+    network_interface_id = aws_network_interface.main[count.index].id    
+  }
   tags = merge(var.common_tags,
     {
       Name = "${var.environment}-instance"
-    }
-  )
-  volume_tags = var.common_tags
+    })
+    
+  volume_tags = merge(var.common_tags, {
+    Name = "${var.environment}-instance-volume"
+  })
 }
